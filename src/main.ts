c2fe8b67bd3aec2f7a376548567d696caf3aa7fd
ebrahim-sani <ibraheemsani035@gmail.com>
2024-10-13 10:17:37 +0100

    update..

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  console.log(
+    `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
+  );
+
   app.enableCors({
     origin: '*',
     methods: ['POST'],
     allowedHeaders: ['Content-Type'],
   });
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
